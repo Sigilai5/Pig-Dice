31992b0dc6ad80a3b8456c5f0f6cfeb7b8bd0b01
Brian Sigilai <briansigiali@gmail.com>
2018-04-12 15:08:48 +0300

    added a stylesheet

---
+++ README.md
@@ -0,0 +1,21 @@
+# Pig Dice Project
+#### This is a Pig Dice game  Website written in HTML5,CSS, Jquery and Javascript.
+#### By Brian K Sigilai
+## 
+## Setup/Installation Requirements
+ * Download the Project.
+ 
+ * Open the index file using a browser.
+ ## How to Play
+ A player rolls a dice first and the results is stored then the second player does the same and the value is stored.Every roll has different values and so the one who gets to score 100 points first is the winner. 
+
+## Technologies Used
+ * HTML
+ * CSS
+ * Javascript
+ * Jquery
+ 
+## Link
+https://sigilai5.github.io/Pig-Dice-Project/
+### License
+Copyright (c) 2018 **Brian K Sigilai**
---
+++ js/scripts.js
@@ -0,0 +1,98 @@
+//this is the backend
+
+function Player(roll,currentScore,totalScore) {
+    this.roll = roll;
+    this.currentScore = currentScore;
+    this.totalScore = totalScore;
+
+}
+
+//Players involved
+var player1 = new Player(0,0,0);
+var player2 = new Player(0,0,0);
+
+//the dice logic
+var dice = function () {
+    return Math.floor(Math.random()* 6) + 1;
+}
+
+//roll button
+Player.prototype.diceOne = function () {
+  if ( this.roll === 1){
+      this.currentScore = 0;
+  }
+
+  else {
+      this.currentScore += this.roll;
+
+  }
+
+}
+
+//hold button
+Player.prototype.hold = function () {
+    this.totalScore += this.currentScore;
+}
+
+//determine the winner
+Player.prototype.winner = function () {
+    if(this.totalScore >= 100) {
+        alert("You are the winner!")
+    }
+}
+
+
+
+//Front-end
+
+//play is clicked
+$(document).ready(function () {
+    $('#play').click(function () {
+        $('.pig-dice').show();
+        $('#play').hide();
+    });
+
+//player1 clicks roll
+    $('#roll1').click(function () {
+        player1.roll = dice();
+        $('.roll1Result').text(player1.roll);
+        player1.diceOne();
+        $('.current1').text(player1.currentScore);
+
+    });
+
+
+//player1 clicks hold
+    $('#hold1').click(function () {
+        player1.hold();
+        $('.total1').text(player1.totalScore);
+        $('.roll1Result').empty();
+        $('.current1').empty();
+        player1.winner();
+        $('#roll1').hide();
+        $('#roll2').show();
+    });
+
+    //player2 clicks roll
+    $('#roll2').click(function () {
+        player2.roll = dice();
+        $('.roll2Result').text(player2.roll);
+        player2.diceOne();
+        $('.current2').text(player2.currentScore);
+
+    });
+
+    //player2 clicks hold
+    $('#hold2').click(function () {
+        player2.hold();
+        $('.total2').text(player2.totalScore);
+        $('.roll2Result').empty();
+        $('.current2').empty();
+        player2.winner();
+        $('#roll2').hide();
+        $('#roll1').show();
+    });
+
+
+
+});
